8e19ce8857c205792e541de3527a450a4e00c58b
captainflashito <f84h5vtvhk@privaterelay.appleid.com>
2026-09-05 09:32:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='ops-center-v3-product-shell-10';
+const CACHE_NAME='ops-center-v3-product-shell-11';
 const APP_FILES=['./','./index.html','./manifest.webmanifest','./ops-icon.svg','./data/airports.min.json','./data/aircraft-catalog.js','./data/world-land.geojson'];
 
 self.addEventListener('install',event=>{
